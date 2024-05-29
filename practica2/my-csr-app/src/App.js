1b265f9337a135957350f552577cd3f4488734e1
Eduardo Barona <beduardogiovanny@yahoo.com>
2024-05-28 21:26:16 -0500

    SSR

---
+++ practica2/my-csr-app/src/App.js
@@ -0,0 +1,12 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello from Client Side Rendering!</h1>
+        </div>
+    );
+}
+
+export default App;
